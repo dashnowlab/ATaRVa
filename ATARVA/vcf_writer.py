2f3aa512da2679d3c92b50474833cb1a26b8df3c
Akshay Avvaru <avvaruakshay@gmail.com>
2026-09-09 13:54:51 -0600

    update version

--- ATARVA/vcf_writer.py
+++ ATARVA/vcf_writer.py
@@ -21,7 +21,7 @@ def vcf_writer(out, bam, bam_name):
     vcf_header = pysam.VariantHeader()
 
     # command
-    vcf_header.add_line(f"##command=ATaRVa_0.7.1+ext {' '.join(sys.argv)}")
+    vcf_header.add_line(f"##command=ATaRVa_0.7.1+ext0.01 {' '.join(sys.argv)}")
 
     for contig in bam.header['SQ']:
         vcf_header.contigs.add(contig['SN'], length=contig['LN'])
